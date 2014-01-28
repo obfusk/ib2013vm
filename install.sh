382c16668e3b04ef8a56415f7dbbca9eb3828cbc
Felix C. Stegerman <flx@obfusk.net>
2014-01-28 22:57:58 +0100

    provision.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,6 @@ box=ib2013vm
 box_url='https://cloud-images.ubuntu.com/vagrant/precise/current/'
 box_url+='precise-server-cloudimg-i386-vagrant-disk1.box'
 
-ib_url='http://download.belastingdienst.nl/belastingdienst/apps/linux/'
-ib_url+='ib2013_linux.tar.gz'
-
-ib_sum='5db3ccfedf3d44ff13ae1ee4009d7e59bb88bf9d4f4d9ba6ac3fca28aaf98e7e'
-ib_sum+='eb670426e4abc2167099279d2151e8721f38f6f58b658cc999e508ae875ea729'
-
 trap 'read -p "press return"' EXIT
 
 if ! test -e id_rsa; then
--- provision.sh
+++ provision.sh
@@ -2,28 +2,16 @@
 
 set -xe
 
+ib_url='http://download.belastingdienst.nl/belastingdienst/apps/linux/'
+ib_url+='ib2013_linux.tar.gz'
+
+ib_sum='5db3ccfedf3d44ff13ae1ee4009d7e59bb88bf9d4f4d9ba6ac3fca28aaf98e7e'
+ib_sum+='eb670426e4abc2167099279d2151e8721f38f6f58b658cc999e508ae875ea729'
+
+ib_tgz=ib2013.tgz
+
 packages=(
-  etckeeper git
-  byobu curl grc htop tree vim
-  build-essential ruby1.9.1-full
-
-  zlib1g-dev libssl-dev libreadline-gplv2-dev
-  libxml2-dev libxslt1-dev
-
-  postgresql-contrib-9.1 postgresql-server-dev-9.1
-  mongodb
-  libsqlite3-dev sqlite3
-  memcached redis-server
-
-  imagemagick libmagickwand-dev
-  nodejs
-
-  nginx-full gitolite
-
-  debian-goodies
-
-  xclip vim-gtk
-  chromium-browser firefox
+  firefox
 )
 
 rm_packages=(
@@ -35,80 +23,15 @@ passwd -l vagrant
 aptitude update
 aptitude -y safe-upgrade
 
-aptitude install -y "${packages[@]}"
+aptitude install -y -R "${packages[@]}"
 aptitude purge -y "${rm_packages[@]}"
-
-update-alternatives --set editor  /usr/bin/vim.basic
-update-alternatives --set ruby    /usr/bin/ruby1.9.1
-update-alternatives --set gem     /usr/bin/gem1.9.1
-
-rm -f /etc/nginx/sites-enabled/default
-
-service nginx restart
-
-(
-  cd /etc
-  if ! test -e .git; then
-    sed -ri '/^VCS="bzr"/ s!^!#!; /VCS="git"/ s!^#!!' \
-      /etc/etckeeper/etckeeper.conf
-    git init
-    git add -A
-    git commit -m ...
-    git gc
-    git status
-  fi
-)
 
 cat <<__END | sed 's!^  !!' | sudo -H -u vagrant bash -xe
   cd
-  mkdir -p bin opt/src
-
-  (
-    cd opt/src
-    for x in sh-config dev-misc map.sh; do
-      test -e "\$x" || \
-      git clone https://github.com/obfusk/"\$x".git
-    done
-  )
-
-  ln -fs opt/src/dev-misc/vimrc       .vimrc
-  ln -fs ../opt/src/map.sh/bin/filter bin/filter
-  ln -fs ../opt/src/map.sh/bin/map    bin/map
-
-  grep -qF prompt.bash .bashrc || \
-  sed 's!^  !!' >> .bashrc <<__END
-
-    . ~/opt/src/sh-config/prompt.bash
-
-  __END
-
-  grep -qF LC_ALL .profile || \
-  sed 's!^  !!' >> .profile <<__END
-
-    for _path in \
-      "\\\$HOME/.gem/ruby/1.9.1/bin" ;
-    do
-      [ -d "\\\$_path" ] && PATH="\\\$_path:\\\$PATH"
-    done
-    unset _path
-    export LC_ALL=C LANG=C GEM_HOME="\\\$HOME/.gem/ruby/1.9.1"
-
-  __END
-
-  grep -qF no-ri .gemrc || \
-  sed 's!^  !!' >> .gemrc <<__END
-    install: --no-rdoc --no-ri
-    update:  --no-rdoc --no-ri
-  __END
-
-  . .profile
-
-  byobu-select-backend screen
-  byobu-ctrl-a screen
-
-  # git config --global user.name ...
-  # git config --global user.email ...
-  git config --global color.ui auto
-
-  gem install bundler pry
+  wget "$ib_url" -O "$ib_tgz"
+  if ! test "\$( sha512sum "$ib_tgz" | awk '{print \$1}' )" = "$ib_sum"; then
+    echo 'sha512sum does not match' >&2
+    exit 1
+  fi
+  tar xf "$ib_tgz"
 __END
